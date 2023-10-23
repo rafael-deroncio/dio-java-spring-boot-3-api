6100aed286bf828fa83e34e90f4397b700b2c60b
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-10-23 01:29:57 -0300

    outra correcao no RestControllerAdvice

--- apicore-service-bank/src/main/java/com/bank/core/handlers/GlobalExceptionHandler.java
+++ apicore-service-bank/src/main/java/com/bank/core/handlers/GlobalExceptionHandler.java
@@ -13,7 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import java.lang.reflect.UndeclaredThrowableException;
 
 @RestControllerAdvice
